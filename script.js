24e2b6ac61cb1f699cbc37ba22c225f9973a2277
lucie-sundermeijer <lucie.sundermeijer@externe.maif.fr>
2024-01-16 11:56:56 +0100

    update for very small size

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function date() {
-    let date = new Date().toLocaleString('fr', { weekday: 'short' }) + ' ' + new Date().toLocaleString('fr', { day: 'numeric' }) + ' ' + new Date().toLocaleString('fr', { month: 'short' }) + ' ' + new Date().toLocaleString('fr', { year: 'numeric' });
+    let date = new Date();
+    if (window.innerWidth > 200) {
+        date = new Date().toLocaleString('fr', { weekday: 'short' }) + ' ' + new Date().toLocaleString('fr', { day: 'numeric' }) + ' ' + new Date().toLocaleString('fr', { month: 'short' }) + ' ' + new Date().toLocaleString('fr', { year: 'numeric' });
+    }
+    else {
+        date = new Date().toLocaleDateString();
+    }
+   
     document.getElementById('date').innerText = date;
 }
 
