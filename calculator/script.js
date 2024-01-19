a1005978f11d6804e4b5056d977cb5c3b86eeda2
Lucie Sundermeijer <lucie.sun@gmail.com>
2024-01-19 18:49:36 +0100

    update + refactor

--- calculator/script.js
+++ calculator/script.js
@@ -1,28 +1,37 @@
 const numberButtons = document.getElementsByClassName('number');
-for (let i = 0; i < numberButtons.length; i++) {
-    numberButtons[i].addEventListener('click', (e) => {
-        document.getElementById("operation").innerText += e.target.innerText
-    })
-}
-
-const operatorButton = document.getElementsByClassName('operator');
-for (let i = 0; i < operatorButton.length; i++) {
-    operatorButton[i].addEventListener('click', (e) => {
-        document.getElementById("operation").innerText += e.target.innerText
-    })
-}
-
+const operatorButtons = document.getElementsByClassName('operator');
 const resultButton = document.getElementsByClassName('equals').item(0);
-resultButton.addEventListener('click', calculate)
-function calculate() {
-    const operation = document.getElementById("operation").innerText
-    const result = eval(operation)
-    document.getElementById("result").innerText = result
+const clearButton = document.getElementsByClassName('clear').item(0);
+
+let shouldClearResult = false;
+
+function addEventListenerToElements(elements, callback) {
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('click', callback);
+    }
 }
 
-const clearButton = document.getElementsByClassName('clear').item(0);
-clearButton.addEventListener('click', clear)
-function clear() {
-    document.getElementById("operation").innerText = ""
-    document.getElementById("result").innerText = ""
+function addToOperation(e) {
+    if (shouldClearResult) {
+        clear();
+        shouldClearResult = false;
+    }
+    document.getElementById("operation").innerText += e.target.innerText;
 }
+
+function calculate() {
+    const operation = document.getElementById("operation").innerText;
+    const result = eval(operation);
+    document.getElementById("result").innerText = result;
+    shouldClearResult = true;
+}
+
+function clear() {
+    document.getElementById("operation").innerText = "";
+    document.getElementById("result").innerText = "";
+}
+
+addEventListenerToElements(numberButtons, addToOperation);
+addEventListenerToElements(operatorButtons, addToOperation);
+resultButton.addEventListener('click', calculate);
+clearButton.addEventListener('click', clear);
